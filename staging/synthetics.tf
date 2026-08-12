ed96dbfc964ee50dc7ec22c0fb386d6da72f94d0
Yusuke TOMIOKA <vlayusuke@gmail.com>
2026-08-12 10:04:04 +0900

    add: ステージング環境のResponse API用Amazon CloudWatch Syntheticsを追加

--- staging/synthetics.tf
+++ staging/synthetics.tf
@@ -61,3 +61,51 @@ data "archive_file" "canary_check_request_api" {
   source_dir  = "${path.cwd}/files/canary/canary-check-request-api"
   output_path = "${path.module}/artifacts/canary-check-request-api.zip"
 }
+
+
+# ===============================================================================
+# Amazon CloudWatch Synthetics Canary for Check Response API Monitoring
+# ===============================================================================
+resource "aws_synthetics_canary" "check_response_api" {
+  name                     = "${local.project}-${local.env}-cwt-syn-check-response-api"
+  artifact_s3_location     = aws_s3_bucket.synthetics_artifacts.arn
+  execution_role_arn       = aws_iam_role.cloudwatch_synthetics.arn
+  handler                  = "function.canary_handler"
+  runtime_version          = "syn-python-selenium-11.1"
+  zip_file                 = data.archive_file.canary_check_response_api.output_path
+  success_retention_period = 31
+  failure_retention_period = 31
+
+  schedule {
+    duration_in_seconds = 0
+    expression          = "rate(3 minutes)"
+  }
+
+  artifact_config {
+    s3_encryption {
+      encryption_mode = "SSE_KMS"
+      kms_key_arn     = aws_kms_key.synthetics.arn
+    }
+  }
+
+  run_config {
+    active_tracing     = false
+    timeout_in_seconds = 60
+    ephemeral_storage  = 2048
+  }
+
+  depends_on = [
+    aws_iam_role.cloudwatch_synthetics,
+    aws_kms_key.synthetics,
+  ]
+
+  tags = {
+    Name = "${local.project}-${local.env}-cwt-syn-check-response-api"
+  }
+}
+
+data "archive_file" "canary_check_response_api" {
+  type        = "zip"
+  source_dir  = "${path.cwd}/files/canary/canary-check-response-api"
+  output_path = "${path.module}/artifacts/canary-check-response-api.zip"
+}
